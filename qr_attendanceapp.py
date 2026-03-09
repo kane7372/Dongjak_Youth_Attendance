6ddeeaae254a045cb7817f7e781052897cac7d20
kane7372 <kane7372@gmail.com>
2026-03-09 23:30:04 +0900

    Update qr_attendanceapp.py

--- qr_attendanceapp.py
+++ qr_attendanceapp.py
@@ -170,7 +170,7 @@ if mode == "admin":
                     col_a, col_b = st.columns(2)
                     with col_a:
                         m_quarter = st.selectbox("쿼터", ["미사", "교리"])
-                        m_grade = st.selectbox("학년", ["선택", "중학교 1학년", "중학교 2학년", "중학교 3학년", "고등학교 1학년", "고등학교 2학년", "고등학교 3학년"])
+                        m_grade = st.selectbox("학년", ["선택", "중학교 1학년", "중학교 2학년", "중학교 3학년", "고등학교 1학년", "고등학교 2학년", "고등학교 3학년","교사"])
                         m_date = st.date_input("출석 날짜 (기본값: 오늘)")
                     with col_b:
                         m_name = st.text_input("이름")
@@ -277,4 +277,5 @@ else:
                         st.success(f"🎊 {grade} {name}({nickname})님, {st.session_state.current_quarter} 출석이 성공적으로 기록되었습니다!")
                 except Exception as e:
                     st.error(f"데이터 저장 중 문제가 발생했습니다. 관리자에게 문의하세요. ({e})")
+
 
